05b8cb6aca8da8faf97df083235e06291dad38ca
yyhsz <591985864@qq.com>
2019-05-13 10:15:33 +0800

    检测g-button-group组件的子元素是否符合规范（必须是button）

---
+++ src/button-group.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        mounted(){
+            for (let node of this.$el.children){
+                if (node.nodeName.toLowerCase() !== 'button'){
+                    console.warn(`g-button 的子元素应该全都是 button 而你使用了 ${node.nodeName.toLowerCase()}`)
+                }
+            }
+        }
+
+    }
+</script>
+<style lang="scss">
+    .g-button-group{
+        display: inline-flex;
+        & :not(:first-child){margin-left:-1px;}
+        > .g-button{border-radius: 0;
+            &:first-child{border-top-left-radius: var(--button-radius);border-bottom-left-radius: var(--button-radius);}
+            &:last-child{border-top-right-radius: var(--button-radius);border-bottom-right-radius: var(--button-radius);}
+            &:hover{
+                position: relative;
+                z-index: 1;
+            }
+        }
+
+
+
+
+    }
+</style>
