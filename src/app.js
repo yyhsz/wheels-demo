08bfa6877f30a6b893b5327c58b09665746b3566
yyhsz <591985864@qq.com>
2019-05-15 17:55:09 +0800

    使用npm run test 和 npx parcel watch test/* --no-chache && npx karma start自动测试

--- src/app.js
+++ src/app.js
@@ -1,16 +1,110 @@
 import Vue from 'vue'
-import Button from  './button.vue'
-import  Icon from  './icon.vue'
+import Button from './button.vue'
+import Icon from './icon.vue'
 import ButtonGroup from './button-group'
+import chai from 'chai'
+import spies from 'chai-spies'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
-Vue.component('g-button-group',ButtonGroup)
+chai.use(spies)
+
+Vue.component('g-button', Button)
+Vue.component('g-button-group', ButtonGroup)
 new Vue({
-    el:'#app',
-    data(){
+    el: '#app',
+    data() {
         return {
-            loading:false,
+            loading: false,
         }
     }
 })
+
+
+// 单元测试
+const expect = chai.expect
+try {
+    {
+        //测试按钮含有icon
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setting'
+            }
+        })
+        vm.$mount()//挂载到内存
+        expect(vm.$el.querySelector('use').getAttribute('xlink:href')).to.eq('#i-setting')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setting',
+                loading: true,
+            }
+        })
+        vm.$mount()
+        expect(vm.$el.querySelector('use').getAttribute('xlink:href')).to.eq('#i-loading')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+
+    {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setting',
+                loading: true,
+            }
+        })
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        let {order} = window.getComputedStyle(svg)
+        expect(order).to.eq('1')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+    {
+        const div = document.createElement('div')
+        document.body.appendChild(div)
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setting',
+                loading: true,
+                iconPosition: 'right',
+            }
+        })
+        vm.$mount(div)
+        let svg = vm.$el.querySelector('svg')
+        let {order} = window.getComputedStyle(svg)
+        expect(order).to.eq('2')
+        vm.$el.remove()
+        vm.$destroy()
+    }
+    {
+        const Constructor = Vue.extend(Button)
+        const vm = new Constructor({
+            propsData: {
+                icon: 'setting',
+                loading: false,
+            }
+        })
+        vm.$mount()
+        //函数的mock
+        let spy = chai.spy(() => {
+        })
+        vm.$on('click', spy)
+        vm.$el.click()
+        expect(spy).to.have.been.called()
+    }
+} catch (error) {
+    window.errors = [error]
+} finally {
+   window.errors && window.errors.forEach((error) => {
+        console.error(error.message)
+    })
+}
+
